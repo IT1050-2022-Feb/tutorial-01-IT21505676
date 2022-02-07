1ff1c2cf58b8107865947a7715454e4281cea731
vikasitha herath <99170233+IT21505676@users.noreply.github.com>
2022-02-07 12:29:07 +0000

    what did you change

--- Tute01.c
+++ Tute01.c
@@ -1,11 +1,20 @@
 /* Exercise 1 - Calculations
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
+#include<stdio.h>
+int main(void){
 
-#include <stdio.h>
+  float mark1,mark2;
+  float avg;
 
-int main() {
-  
+  printf("Enter mark 1 :");
+  scanf("%f",&mark1);
+  printf("Enter mark 2 :");
+  scanf("%f",&mark2);
+
+  avg=(mark1+mark2)/2.0;
+  printf("Average  :  %.2f",avg);
   return 0;
 }
+
 
--- Tute04.c
+++ Tute04.c
@@ -6,14 +6,37 @@ Do not change the code given in the main() function when you are implementing yo
 
 #include <stdio.h>
 
+int minimum(int a,int b);
+int maximum(int c,int d);
+int multiply(int e,int f);
+
 int main() {
    int no1, no2;
    printf("Enter a value for no 1 : ");
    scanf("%d", &no1);
    printf("Enter a value for no 2 : ");
    scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
+   printf("%d \n", minimum(no1, no2));
+   printf("%d \n", maximum(no1, no2));
+   printf("%d \n", multiply(no1, no2));
    return 0;
+}
+int minimum(int a,int b){
+  if(a>b){
+    return b;
+  }
+  else{
+    return a;
+  }
+}
+int maximum(int c,int d){
+  if(c>d){
+    return c;
+  }
+  else{
+    return d;
+  }
+}
+int multiply(int e,int f){
+  return e*f;
 }
